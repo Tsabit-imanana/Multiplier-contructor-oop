fed06d72917c33b7056f56bf044c9e878da7ec80
Tsabit-imanana <69383628+Tsabit-imanana@users.noreply.github.com>
2025-03-09 23:08:59 +0700

    Update Balok.java

--- Balok.java
+++ Balok.java
@@ -1,31 +1,31 @@
-public class Balok {
+class Balok {
+    private double panjang, lebar, tinggi;
 
-    public double panjang, lebar, tinggi;
+    public Balok() {
+        this.panjang = 1.0;
+        this.lebar = 1.0;
+        this.tinggi = 1.0;
+    }
 
-    // using set
+    public Balok(double panjang, double lebar, double tinggi) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.tinggi = tinggi;
+    }
+
     public void setPanjang(double panjang) {
         this.panjang = panjang;
     }
+
     public void setLebar(double lebar) {
         this.lebar = lebar;
     }
+
     public void setTinggi(double tinggi) {
         this.tinggi = tinggi;
     }
 
-    // using multiple constructor
-    Balok() {
-
-    }
-
-    Balok(double panjang, double lebar, double tinggi) {
-        this.panjang = panjang;
-        this.lebar = lebar;
-        this.tinggi = tinggi;
-    }
-
-    public double ComputeAndSetVolume() {
-        double volume = this.panjang * this.lebar * this.tinggi;
-        return volume;
+    public double hitungVolume() {
+        return panjang * lebar * tinggi;
     }
 }
